384918ed238f242c7a1fa8e68beba17fb5dfad5d
Artem Monkiewicz <artem.monkiewicz@gmail.com>
2024-09-01 18:25:55 +0200

    Day 1: Basics of Python - print, input, variable naming, and first bot
    - Practiced basic Python methods: print() and input()
    - Reviewed common variable naming mistakes
    - Created a basic bot that generates a band name based on a city name and pet's name

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello! Welcome to the Band Name Generator!")
+city = input("What's the name of the city you gre up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
