97f6de79fd28a213063d1e66359ce6e397ebbaa0
Artem Monkiewicz <artem.monkiewicz@gmail.com>
2025-01-20 19:05:47 +0100

    Implement bidding functionality and display highest bidder in Blind Auction Project

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -1,6 +1,18 @@
-# TODO-1: Ask the user for input
+# TODO-1: Ask the user for input and bid
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
 
+bids = {}
+continue_bidding = True
 
+while continue_bidding == True:
+    name = input("What is your name? ")
+    price = int(input("What is your bid? $"))
+    bids[name] = price
+    print(bids)
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if should_continue == "no":
+        break
+
+print(bids)
